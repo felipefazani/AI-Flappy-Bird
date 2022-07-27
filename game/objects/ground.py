2816a8015f7f5e21dae8a7f55ba2426d7714cde0
Felipe Fazani <lipefazani@gmail.com>
2022-07-27 08:57:42 -0300

    create a ground class

---
+++ game/objects/ground.py
@@ -0,0 +1,21 @@
+from pygame import Surface
+from game.config import Images, Ground_config
+
+
+class Ground:
+   def __init__(self, x_start_position, y_start_position) -> None:
+      self.x = x_start_position
+      self.y = y_start_position
+      self.ground_width = Images.GROUND.value.get_width()
+
+   def move(self) -> None:
+      self.x -= Ground_config.SPEED.value
+
+      if self.x + self.ground_width < 0:
+         self.x += + self.ground_width
+
+   def draw(self, surface : Surface) -> None:
+      surface.blit(
+         source=Images.GROUND.value, 
+         dest=(self.x, self.y)
+      )
