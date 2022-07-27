0a6ddfa0dd661ffd86bfa20d76c1e75837722347
Felipe Fazani <lipefazani@gmail.com>
2022-07-27 08:41:41 -0300

    create a pipe class

---
+++ game/objects/pipe.py
@@ -0,0 +1,48 @@
+import pygame
+from random import randrange
+from game.config import Images, Pipe_config
+from game.objects.bird import Bird
+
+
+class Pipe:
+   def __init__(self, x_start_position) -> None:
+      self.x = x_start_position
+      self.top_pipe = pygame.transform.flip(
+         surface=Images.PIPE.value, 
+         flip_x=False, 
+         flip_y= True
+      )
+      self.ground_pipe = Images.PIPE.value
+      self.left_screen = False
+
+      self.height = randrange(start=25, stop=475)
+      self.top_position = self.height - self.top_pipe.get_height()
+      self.ground_position = self.height + Pipe_config.DISTANCE.value['max']
+
+   def move(self) -> None:
+      self.x -= Pipe_config.SPEED.value
+
+   def draw(self, surface : pygame.Surface) -> None:
+      surface.blit(source=self.top_pipe, dest=(self.x, self.top_position))
+      surface.blit(source=self.ground_pipe, dest=(self.x, self.ground_position))
+
+   def colision(self, bird : Bird) -> bool:
+      bird_mask = bird.get_mask()
+      top_mask = pygame.mask.from_surface(surface=self.top_pipe)
+      ground_mask = pygame.mask.from_surface(surface=self.ground_pipe)
+
+      x_distance = self.x - bird.x
+      top_colision = bird_mask.overlap(
+         top_mask, 
+         (x_distance, self.top_position - bird.y)
+      )
+      ground_colision = bird_mask.overlap(
+         ground_mask, 
+         (x_distance, self.ground_position - bird.y)
+      )
+
+      if ground_colision or top_colision:
+         return True
+      else:
+         return False
+
